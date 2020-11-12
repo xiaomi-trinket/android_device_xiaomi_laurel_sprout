e35c230b662e3d98dad62a3c96840a95adc6b627
Vishalcj17 <vishalcj@aospa.co>
2020-11-12 12:53:17 +0530

    laurel_sprout: import fstab from stock.
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_PACKAGES += \
     libspkrprot \
     libtinycompress.vendor
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
